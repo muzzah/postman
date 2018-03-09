929a6c7d7b0a2066f26ea809deefec2b93b46ad5
Mustafa Sezgin <mustafa@siia.io>
2018-03-09 18:15:57 +0100

    Fixed Bar Graph to display and update appropriately.Updates to joining class and quiz responses working. Services are foregrounded as soon as they start. Storing quiz responses and students who connect to classroom in shared preferences

--- src/main/java/com/siia/postman/server/PostmanServer.java
+++ src/main/java/com/siia/postman/server/PostmanServer.java
@@ -1,5 +1,6 @@
 package com.siia.postman.server;
 
+import android.support.annotation.AnyThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.WorkerThread;
 
@@ -13,20 +14,28 @@ import io.reactivex.subjects.PublishSubject;
 public interface PostmanServer {
 
 
+    @AnyThread
     void serverStart();
 
     @WorkerThread
     void stopServer();
 
+    @AnyThread
     boolean isRunning();
 
+    @AnyThread
     PublishSubject<ServerEvent> getServerEventsStream();
 
+    @AnyThread
     void broadcastMessage(PostmanMessage msg);
 
+    @AnyThread
     void sendMessage(@NonNull  PostmanMessage msg, @NonNull Connection client);
+
+    @AnyThread
     void sendMessage(@NonNull MessageLite message, @NonNull Connection client);
 
+    @AnyThread
     int numberOfClients();
 
     @WorkerThread
